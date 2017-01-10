4d84faf18e6e9f2cebc1ea6a808cede7b477848b
TheJP <jp_55555@hotmail.com>
2017-01-10 23:03:48 +0100

    Fixed validation of add adventurers form

--- src/DnD/Models/AdventureViewModels/AddAdventurersViewModel.cs
+++ src/DnD/Models/AdventureViewModels/AddAdventurersViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DnD.Data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,12 +7,27 @@ using System.Threading.Tasks;
 
 namespace DnD.Models.AdventureViewModels
 {
-    public class AddAdventurersViewModel
+    public class AddAdventurersViewModel : IValidatableObject
     {
         [Display(Name = "Adventure")]
         public int AdventureId { get; set; }
 
         /// <summary>Character ids of adventurers which will be added.</summary>
         public IEnumerable<int> Adventurers { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var context = (ApplicationDbContext)validationContext.GetService(typeof(ApplicationDbContext));
+            if (!context.Adventures.Any(a => a.Id == AdventureId))
+            {
+                yield return new ValidationResult($"An unkown adventure was selected.", new[] { "AdventureId" });
+            }
+
+            //Linq to SQL should optimise this to a "WHERE IN" query
+            if (context.Characters.Any(c => !Adventurers.Contains(c.Id)))
+            {
+                yield return new ValidationResult($"One or more unkown adventurers.", new[] { "Adventures" });
+            }
+        }
     }
 }
